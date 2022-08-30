e3d3396086e83b3e83ced0379de2efceed1de2cf
Rafa Lopez <alexander20031127@outlook.com>
2022-08-29 22:13:40 -0600

    Add project files.

---
+++ Practica 4.3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Practica_4._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lista = new List<string>();
+            Console.WriteLine("Cantidad: {0}", lista.Count);
+            Console.WriteLine("Capacidad: {0}", lista.Capacity);
+            Console.WriteLine();
+
+            for (int i = 0; i < 10; i++)
+            {
+                lista.Add(" Dato " + i);
+            }
+            Console.WriteLine("Cantidad: {0}", lista.Count);
+            Console.WriteLine("Capacidad: {0}", lista.Capacity);
+            Console.WriteLine();
+
+
+            for (int i = 10; i < 33; i++)
+            {
+                lista.Add(" Dato " + i);
+            }
+            Console.WriteLine("Cantidad: {0}", lista.Count);
+            Console.WriteLine("Capacidad: {0}", lista.Capacity);
+            Console.WriteLine();
+
+            foreach (string row in lista)
+            {
+                Console.WriteLine(row);
+            }
+
+            Console.WriteLine("=========== Eliminar rango ==========");
+            lista.RemoveRange(5, 4);
+
+
+            foreach (string row in lista)
+            {
+                Console.WriteLine(row);
+            }
+
+            lista.Insert(11, "Elemento nuevo");
+
+            foreach (string row in lista)
+            {
+                Console.WriteLine(row);
+            }
+
+            lista.Clear();
+            Console.WriteLine("Cantidad: {0}", lista.Count);
+            Console.WriteLine("Capacidad: {0}", lista.Capacity);
+        }
+    }
+}
